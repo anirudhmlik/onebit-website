a5726625e6333774ce627644408344183a41b21d
anirudhmalik-ops <anirudhchoudhary308@gmail.com>
2025-11-01 23:22:34 +0530

    cto

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigation } from "@/components/Navigation";
 import { RetroCursor } from "@/components/RetroCursor";
-import { LoadingScreen } from "@/components/LoadingScreen";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Products from "./pages/Products";
@@ -18,7 +17,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <LoadingScreen />
       <RetroCursor />
       <Toaster />
       <Sonner />
